07fb4e75577add701881e8f1365ce8ad3532f279
Glenn Cai <glenn.lh.cai@gmail.com>
2023-08-18 20:57:05 +0800

    feat: handle custom exception for the feign client

---
+++ order/src/main/java/com/dailycodebuffer/order/external/config/FeignConfig.java
@@ -0,0 +1,7 @@
+package com.dailycodebuffer.order.external.config;/**
+  * This class is for xxx.
+  *
+  * @author Glenn Cai
+  * @version 1.0 18/8/2023
+  */public class FeignConfig {
+}
---
+++ order/src/main/java/com/dailycodebuffer/order/external/model/ErrorResponse.java
@@ -0,0 +1,7 @@
+package com.dailycodebuffer.order.external.model;/**
+  * This class is for xxx.
+  *
+  * @author Glenn Cai
+  * @version 1.0 18/8/2023
+  */public class ErrorResponse {
+}
